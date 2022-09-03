3a10d27e243d0a2cba9b2921bbdc28c06d4ee1da
ShivamRathod0110 <102019556+ShivamRathod0110@users.noreply.github.com>
2022-09-03 18:28:20 +0530

    Update code.py

--- YouTube Video Downnloader/code.py
+++ YouTube Video Downnloader/code.py
@@ -1,1 +1,17 @@
+from pytube import YouTube
 
+url = input("Enter the youtube link URL: ")
+yt = YouTube(url) #Fetch the video
+
+print(f"Title: {yt.title}")
+videos = yt.streams
+
+#Enumerate the fetched video and print it out.
+for i,video in enumerate(videos):
+    print(f"{str(i)} {str(video)}")
+
+num = int(input("Enter your choice: ")) #For video formats/sizes
+video = videos[num]
+
+print("Downloading the video. Please wait...")
+video.download()
